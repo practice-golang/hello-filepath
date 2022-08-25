2690302484c3e41ce70341d7231683badb2565e5
Practice Golang <practice.golang@gmail.com>
2022-08-25 15:13:48 +0900

    Correct test

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
 build:
 	go build -ldflags "-w -s -H windowsgui" -o bin/
+
+test:
+	go test -v
--- main_test.go
+++ main_test.go
@@ -10,14 +10,17 @@ import (
 )
 
 type MockFileInfo struct {
-	FileName    string
-	IsDirectory bool
+	FileName         string
+	IsDirectory      bool
+	CreateModifyTime time.Time
 }
 
-func (mf MockFileInfo) Name() string       { return mf.FileName }
-func (mf MockFileInfo) Size() int64        { return int64(8) }
-func (mf MockFileInfo) Mode() os.FileMode  { return os.ModePerm }
-func (mf MockFileInfo) ModTime() time.Time { return time.Now() }
+func (mf MockFileInfo) Name() string      { return mf.FileName }
+func (mf MockFileInfo) Size() int64       { return int64(8) }
+func (mf MockFileInfo) Mode() os.FileMode { return os.ModePerm }
+
+// func (mf MockFileInfo) ModTime() time.Time { return time.Now() }
+func (mf MockFileInfo) ModTime() time.Time { return mf.CreateModifyTime }
 func (mf MockFileInfo) IsDir() bool        { return mf.IsDirectory }
 func (mf MockFileInfo) Sys() interface{}   { return nil }
 
@@ -26,36 +29,41 @@ func Test_sortByName(t *testing.T) {
 		a fs.FileInfo
 		b fs.FileInfo
 	}
-	tests := []struct {
+	type testInst struct {
 		name string
 		args args
 		want bool
-	}{
-		{
-			name: "Test_sortByName_files",
-			args: args{
-				a: MockFileInfo{FileName: "a", IsDirectory: false},
-				b: MockFileInfo{FileName: "b", IsDirectory: false},
-			},
-			want: true,
-		},
-		{
-			name: "Test_sortByName_files_dir_file",
-			args: args{
-				a: MockFileInfo{FileName: "a", IsDirectory: true},
-				b: MockFileInfo{FileName: "b", IsDirectory: false},
-			},
-			want: true,
-		},
-		{
-			name: "Test_sortByName_files_file_dir",
-			args: args{
-				a: MockFileInfo{FileName: "a", IsDirectory: true},
-				b: MockFileInfo{FileName: "b", IsDirectory: false},
-			},
-			want: true,
-		},
 	}
+
+	var tests []testInst
+
+	tests = append(tests, testInst{
+		name: "Test_sortByName_files",
+		args: args{
+			a: MockFileInfo{FileName: "a", IsDirectory: false},
+			b: MockFileInfo{FileName: "b", IsDirectory: false},
+		},
+		want: true,
+	})
+
+	tests = append(tests, testInst{
+		name: "Test_sortByName_files_dir_file",
+		args: args{
+			a: MockFileInfo{FileName: "a", IsDirectory: true},
+			b: MockFileInfo{FileName: "b", IsDirectory: false},
+		},
+		want: true,
+	})
+
+	tests = append(tests, testInst{
+		name: "Test_sortByName_files_file_dir",
+		args: args{
+			a: MockFileInfo{FileName: "a", IsDirectory: true},
+			b: MockFileInfo{FileName: "b", IsDirectory: false},
+		},
+		want: true,
+	})
+
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			got := sortByName(tt.args.a, tt.args.b)
@@ -69,36 +77,44 @@ func Test_sortByTime(t *testing.T) {
 		a fs.FileInfo
 		b fs.FileInfo
 	}
-	tests := []struct {
+	type testInst struct {
 		name string
 		args args
 		want bool
-	}{
-		{
-			name: "Test_sortByTime_files",
-			args: args{
-				a: MockFileInfo{FileName: "a", IsDirectory: false},
-				b: MockFileInfo{FileName: "b", IsDirectory: false},
-			},
-			want: false,
-		},
-		{
-			name: "Test_sortByTime_files_dir_file",
-			args: args{
-				a: MockFileInfo{FileName: "a", IsDirectory: true},
-				b: MockFileInfo{FileName: "b", IsDirectory: false},
-			},
-			want: true,
-		},
-		{
-			name: "Test_sortByTime_files_file_dir",
-			args: args{
-				a: MockFileInfo{FileName: "a", IsDirectory: true},
-				b: MockFileInfo{FileName: "b", IsDirectory: false},
-			},
-			want: true,
-		},
 	}
+
+	t1, _ := time.Parse(time.RFC3339, "2022-08-24T22:08:41+00:00")
+	t2, _ := time.Parse(time.RFC3339, "2022-08-25T22:08:41+00:00")
+
+	var tests []testInst
+
+	tests = append(tests, testInst{
+		name: "Test_sortByTime_files",
+		args: args{
+			a: MockFileInfo{FileName: "a", IsDirectory: false, CreateModifyTime: t2},
+			b: MockFileInfo{FileName: "b", IsDirectory: false, CreateModifyTime: t1},
+		},
+		want: false,
+	})
+
+	tests = append(tests, testInst{
+		name: "Test_sortByTime_files_dir_file",
+		args: args{
+			a: MockFileInfo{FileName: "a", IsDirectory: true, CreateModifyTime: t1},
+			b: MockFileInfo{FileName: "b", IsDirectory: false, CreateModifyTime: t2},
+		},
+		want: true,
+	})
+
+	tests = append(tests, testInst{
+		name: "Test_sortByTime_files_file_dir",
+		args: args{
+			a: MockFileInfo{FileName: "a", IsDirectory: true, CreateModifyTime: t1},
+			b: MockFileInfo{FileName: "b", IsDirectory: false, CreateModifyTime: t2},
+		},
+		want: true,
+	})
+
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			got := sortByTime(tt.args.a, tt.args.b)
@@ -113,120 +129,117 @@ func Test_getDir(t *testing.T) {
 		sortby    int
 		direction int
 	}
-	tests := []struct {
+
+	type testInst struct {
 		name string
 		args args
 		want []fs.FileInfo
-	}{
-		{
-			name: "Test_getDir_name_asc",
-			args: args{
-				path:      "./",
-				sortby:    NAME,
-				direction: ASC,
-			},
-			want: []fs.FileInfo{
-				MockFileInfo{FileName: ".git", IsDirectory: true},
-				MockFileInfo{FileName: "bin", IsDirectory: true},
-				MockFileInfo{FileName: ".gitignore", IsDirectory: false},
-				MockFileInfo{FileName: "README.md", IsDirectory: false},
-				MockFileInfo{FileName: "go.mod", IsDirectory: false},
-				MockFileInfo{FileName: "go.sum", IsDirectory: false},
-				MockFileInfo{FileName: "main.go", IsDirectory: false},
-				MockFileInfo{FileName: "main_test.go", IsDirectory: false},
-			},
-		},
-		{
-			name: "Test_getDir_name_desc",
-			args: args{
-				path:      "./",
-				sortby:    NAME,
-				direction: DESC,
-			},
-			want: []fs.FileInfo{
-				MockFileInfo{FileName: "main_test.go", IsDirectory: false},
-				MockFileInfo{FileName: "main.go", IsDirectory: false},
-				MockFileInfo{FileName: "go.sum", IsDirectory: false},
-				MockFileInfo{FileName: "go.mod", IsDirectory: false},
-				MockFileInfo{FileName: "README.md", IsDirectory: false},
-				MockFileInfo{FileName: ".gitignore", IsDirectory: false},
-				MockFileInfo{FileName: "bin", IsDirectory: true},
-				MockFileInfo{FileName: ".git", IsDirectory: true},
-			},
-		},
-		{
-			name: "Test_getDir_size_asc",
-			args: args{
-				path:      "./",
-				sortby:    SIZE,
-				direction: ASC,
-			},
-			want: []fs.FileInfo{
-				MockFileInfo{FileName: ".git", IsDirectory: true},
-				MockFileInfo{FileName: "bin", IsDirectory: true},
-				MockFileInfo{FileName: ".gitignore", IsDirectory: false},
-				MockFileInfo{FileName: "README.md", IsDirectory: false},
-				MockFileInfo{FileName: "go.mod", IsDirectory: false},
-				MockFileInfo{FileName: "go.sum", IsDirectory: false},
-				MockFileInfo{FileName: "main.go", IsDirectory: false},
-				MockFileInfo{FileName: "main_test.go", IsDirectory: false},
-			},
-		},
-		{
-			name: "Test_getDir_size_desc",
-			args: args{
-				path:      "./",
-				sortby:    SIZE,
-				direction: DESC,
-			},
-			want: []fs.FileInfo{
-				MockFileInfo{FileName: "main_test.go", IsDirectory: false},
-				MockFileInfo{FileName: "main.go", IsDirectory: false},
-				MockFileInfo{FileName: "go.sum", IsDirectory: false},
-				MockFileInfo{FileName: "go.mod", IsDirectory: false},
-				MockFileInfo{FileName: "README.md", IsDirectory: false},
-				MockFileInfo{FileName: ".gitignore", IsDirectory: false},
-				MockFileInfo{FileName: ".git", IsDirectory: true},
-				MockFileInfo{FileName: "bin", IsDirectory: true},
-			},
-		},
-		{
-			name: "Test_getDir_time_asc",
-			args: args{
-				path:      "./",
-				sortby:    TIME,
-				direction: ASC,
-			},
-			want: []fs.FileInfo{
-				MockFileInfo{FileName: ".git", IsDirectory: true},
-				MockFileInfo{FileName: "bin", IsDirectory: true},
-				MockFileInfo{FileName: ".gitignore", IsDirectory: false},
-				MockFileInfo{FileName: "README.md", IsDirectory: false},
-				MockFileInfo{FileName: "go.mod", IsDirectory: false},
-				MockFileInfo{FileName: "go.sum", IsDirectory: false},
-				MockFileInfo{FileName: "main.go", IsDirectory: false},
-				MockFileInfo{FileName: "main_test.go", IsDirectory: false},
-			},
-		},
-		{
-			name: "Test_getDir_time_desc",
-			args: args{
-				path:      "./",
-				sortby:    TIME,
-				direction: DESC,
-			},
-			want: []fs.FileInfo{
-				MockFileInfo{FileName: "main_test.go", IsDirectory: false},
-				MockFileInfo{FileName: "main.go", IsDirectory: false},
-				MockFileInfo{FileName: "go.sum", IsDirectory: false},
-				MockFileInfo{FileName: "go.mod", IsDirectory: false},
-				MockFileInfo{FileName: "README.md", IsDirectory: false},
-				MockFileInfo{FileName: ".gitignore", IsDirectory: false},
-				MockFileInfo{FileName: "bin", IsDirectory: true},
-				MockFileInfo{FileName: ".git", IsDirectory: true},
-			},
-		},
 	}
+
+	var tests []testInst
+
+	tests = append(tests, testInst{
+		name: "Test_getDir_name_asc",
+		args: args{
+			path:      "./",
+			sortby:    NAME,
+			direction: ASC,
+		},
+		want: []fs.FileInfo{
+			MockFileInfo{FileName: ".git", IsDirectory: true},
+			MockFileInfo{FileName: "bin", IsDirectory: true},
+			MockFileInfo{FileName: ".gitignore", IsDirectory: false},
+			MockFileInfo{FileName: "go.mod", IsDirectory: false},
+			MockFileInfo{FileName: "go.sum", IsDirectory: false},
+		},
+	})
+
+	tests = append(tests, testInst{
+		name: "Test_getDir_name_desc",
+		args: args{
+			path:      "./",
+			sortby:    NAME,
+			direction: DESC,
+		},
+		want: []fs.FileInfo{
+			MockFileInfo{FileName: "bin", IsDirectory: true},
+			MockFileInfo{FileName: ".git", IsDirectory: true},
+			MockFileInfo{FileName: "request.http", IsDirectory: false},
+			MockFileInfo{FileName: "README.md", IsDirectory: false},
+			MockFileInfo{FileName: "Makefile", IsDirectory: false},
+		},
+	})
+
+	tests = append(tests, testInst{
+		name: "Test_getDir_size_asc",
+		args: args{
+			path:      "./",
+			sortby:    SIZE,
+			direction: ASC,
+		},
+		want: []fs.FileInfo{
+			MockFileInfo{FileName: ".git", IsDirectory: true},
+			MockFileInfo{FileName: "bin", IsDirectory: true},
+			MockFileInfo{FileName: ".gitignore", IsDirectory: false},
+			MockFileInfo{FileName: "README.md", IsDirectory: false},
+			MockFileInfo{FileName: "Makefile", IsDirectory: false},
+		},
+	})
+
+	tests = append(tests, testInst{
+		name: "Test_getDir_size_desc",
+		args: args{
+			path:      "./",
+			sortby:    SIZE,
+			direction: DESC,
+		},
+		want: []fs.FileInfo{
+			MockFileInfo{FileName: "main.go", IsDirectory: false},
+			MockFileInfo{FileName: "main_test.go", IsDirectory: false},
+			MockFileInfo{FileName: "go.sum", IsDirectory: false},
+			MockFileInfo{FileName: "index.html", IsDirectory: false},
+			MockFileInfo{FileName: "go.mod", IsDirectory: false},
+		},
+	})
+
+	t1, _ := time.Parse(time.RFC3339, "2022-08-18T22:08:41+00:00")
+	t2 := t1.AddDate(0, 0, 1)
+	t3 := t1.AddDate(0, 0, 1)
+	t4 := t1.AddDate(0, 0, 1)
+	t5 := t1.AddDate(0, 0, 1)
+
+	tests = append(tests, testInst{
+		name: "Test_getDir_time_asc",
+		args: args{
+			path:      "./",
+			sortby:    TIME,
+			direction: ASC,
+		},
+		want: []fs.FileInfo{
+			MockFileInfo{FileName: ".gitignore", IsDirectory: false, CreateModifyTime: t3},
+			MockFileInfo{FileName: "request.http", IsDirectory: false, CreateModifyTime: t5},
+			MockFileInfo{FileName: "README.md", IsDirectory: false, CreateModifyTime: t4},
+			MockFileInfo{FileName: "go.mod", IsDirectory: true, CreateModifyTime: t1},
+			MockFileInfo{FileName: "go.sum", IsDirectory: true, CreateModifyTime: t2},
+		},
+	})
+
+	tests = append(tests, testInst{
+		name: "Test_getDir_time_desc",
+		args: args{
+			path:      "./",
+			sortby:    TIME,
+			direction: DESC,
+		},
+		want: []fs.FileInfo{
+			MockFileInfo{FileName: "main_test.go", IsDirectory: false, CreateModifyTime: t1},
+			MockFileInfo{FileName: "Makefile", IsDirectory: true, CreateModifyTime: t5},
+			MockFileInfo{FileName: ".git", IsDirectory: true, CreateModifyTime: t4},
+			MockFileInfo{FileName: "bin", IsDirectory: true, CreateModifyTime: t3},
+			MockFileInfo{FileName: "main.go", IsDirectory: false, CreateModifyTime: t2},
+		},
+	})
+
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			os.Chtimes("./main_test.go", time.Now(), time.Now())
@@ -237,6 +250,9 @@ func Test_getDir(t *testing.T) {
 			}
 
 			for i, f := range got {
+				if i > (len(tt.want) - 1) {
+					break
+				}
 				require.Equal(t, tt.want[i].Name(), f.Name())
 			}
 		})
